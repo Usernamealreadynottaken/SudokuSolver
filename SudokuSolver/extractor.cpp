16881af9abcd9b55b4fb2998b3abf854e9c2c88c
unknown <vfurball@gmail.com>
2015-03-19 12:01:44 +0100

    Improve edge detection.

--- SudokuSolver/extractor.cpp
+++ SudokuSolver/extractor.cpp
@@ -4,7 +4,10 @@ Extractor::Extractor() : low_threshold(10), high_threshold(50), kernel_size(3) {
 
 void Extractor::extract(cv::Mat image)
 {
+	// double blur to remove noise
 	blur( image, detected_edges, cv::Size(3,3) );
+	blur( detected_edges, detected_edges, cv::Size(3,3) );
+	// canny to detect edges
 	cv::Canny(detected_edges, detected_edges, low_threshold, high_threshold, kernel_size);
 
 	cv::Mat dst;
